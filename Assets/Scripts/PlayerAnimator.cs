cd00f092ca61e9b1ec809354fa1389dcb751b0fe
Dormin92 <45270590+Dormin92@users.noreply.github.com>
2019-11-01 23:15:58 +0000

    First Commit

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxis("ForwardBack");
+        Animator.SetFloat("Speed", speed);
+
+        if (Input.GetMouseButtonDown(0))
+            Animator.SetTrigger("Attack");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    public float turnSpeed = 1;
+
+    private bool swordActiveFrames = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotation = new Vector3(0f, (Input.GetAxis("Turning") * turnSpeed * Time.deltaTime), 0f);
+        transform.Rotate(rotation);
+
+        if(swordActiveFrames)
+            Debug.Log("Hitframes!");
+    }
+
+    void Hit(int activation)
+    {
+        if (activation == 1)
+            swordActiveFrames = true;
+        else
+            swordActiveFrames = false;
+    }
+}
